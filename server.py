5ebfd02c092e1ba2f8c9f0a0f4c92a141a1b0730
bigdatastory <52017445+bigdatastory@users.noreply.github.com>
2021-06-21 10:28:22 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -19,26 +13,8 @@ def Keyboard():
 @app.route('/message', methods=['POST'])
 def Message():
     content = request.get_json()
-    print(content)
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-@app.route('/keyboard')
-def Keyboard():
-    dataSend = {
-      "user" : "has3ong",
-      "blog" : "github",
-    }
-    return jsonify(dataSend)
-
-@app.route('/message', methods=['POST'])
-def Message():
-    content = request.get_json()
-    print(content)
     content = content['userRequest']
     content = content['utterance']
-    #content = content['user_body']
     
     dataSend = {
         "version": "2.0",
@@ -62,27 +38,3 @@ def Message():
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
-    dataSend = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : content
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-    }
-    return jsonify(dataSend)
-    
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
