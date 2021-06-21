6bf0280f7a57373c4c1beb0d2b2524761695abaf
bigdatastory <52017445+bigdatastory@users.noreply.github.com>
2021-06-21 10:25:54 +0900

    Update server.py

--- server.py
+++ server.py
@@ -20,10 +20,48 @@ def Keyboard():
 def Message():
     content = request.get_json()
     print(content)
-    content = content['detailParams']
-    print('detailParams')
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+      "user" : "has3ong",
+      "blog" : "github",
+    }
+    return jsonify(dataSend)
+
+@app.route('/message', methods=['POST'])
+def Message():
+    content = request.get_json()
     print(content)
-    content = content['user_symptom']
+    #content = content['userRequest']
+    #content = content['utterance']
+    #content = content['user_body']
+    
+    dataSend = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "carousel": {
+                        "type" : "basicCard",
+                        "items": [
+                            {
+                                "title" : "",
+                                "description" : content
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(dataSend)
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
     
     dataSend = {
         "version": "2.0",
