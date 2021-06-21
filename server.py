a5b65650d68b2601cdab4be9db9ff28b5fac5da8
bigdatastory <52017445+bigdatastory@users.noreply.github.com>
2021-06-21 10:16:46 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+      "user" : "has3ong",
+      "blog" : "github",
+    }
+    return jsonify(dataSend)
+
+@app.route('/message', methods=['POST'])
+def Message():
+    content = request.get_json()
+    print(content)
+    #content = content['userRequest']
+    content = content['user_symptom']
+    
+    dataSend = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "carousel": {
+                        "type" : "basicCard",
+                        "items": [
+                            {
+                                "title" : "",
+                                "description" : content
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(dataSend)
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
